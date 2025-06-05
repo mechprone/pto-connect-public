5a5ab4e6bc6edcc1cfa3219f3ff1301b7529293b
mechprone <mvalzan@gmail.com>
2025-06-05 11:03:26 -0500

    Claude suggestions

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{vue,html}",
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
 }
