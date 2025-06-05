ec6cafd6314011e9d23c010afb00926289b2b248
mechprone <mvalzan@gmail.com>
2025-06-05 10:07:32 -0500

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
-// postcss.config.cjs
-import tailwindcss from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
-
 export default {
-  plugins: [tailwindcss(), autoprefixer()],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 }
